3a70b538b845abd22a20266b18504ed2879d1040
Alex DeGrave <ajd98@pitt.edu>
2017-12-14 07:51:09 -0800

    Added comments and documentation

--- flood.pyx
+++ flood.pyx
@@ -2,16 +2,81 @@ cimport cython
 cimport numpy
 import numpy
 from libc.math cimport floor, ceil, sqrt
+'''
+Make sure that the solute is whole (rather than split over a periodic boundary)
+before running this.
+'''
 
-cpdef int volume(numpy.ndarray[numpy.float64_t, ndim=2] _solute_pos, 
-                 numpy.ndarray[numpy.float64_t, ndim=1] _solute_rad, 
-                 numpy.ndarray[numpy.float64_t, ndim=2] _solvent_pos, 
-                 double _solvent_rad,
-                 double _voxel_len):
+cdef bool is_free(double voxx, double voxy, double voxz,
+        double[:,:] solute_pos, double[:] solute_rad,
+        int nsolute, double solvent_rad):
+    '''
+    ---------------
+    Parameters
+    ---------------
+    voxx: (double) the x-position of the voxel
+    voxy: (double) the y-position of the voxel
+    voxz: (double) the z-position of the voxel
+    solute_pos: shape (n,3) array of positions of solute atoms
+    solute_rad: shape (n,) array of radii of solute atoms
+    nsolute: (int) The value ``n`` in the above two lines, ie, the number of
+        solute atoms.
+    solvent_rad: (double) the radius of the solvent 
+
+    --------
+    Returns
+    -------
+    True if a sphere of radius ``solvent_rad`` centered at (voxx, voxy, voxz) 
+    does not intersect any sphere of radius solute_rad[i] centered at 
+    solute_pos[i], for any i=1...n
+
+    False otherwise
     '''
 
+    cdef bool check = True
+
+    for isolute in range(nsolute):
+        solx = solute_pos[isolute,0]
+        soly = solute_pos[isolute,1]
+        solz = solute_pos[isolute,2]
+        solrad = solute_rad[isolute]
+        if dist(x,y,z,solx,soly,solz) < solrad + solvent_rad:
+            check = False
+            break
+    return check
+
+
+cdef double dist(double x1, double y1, double z1, 
+                 double x2, double y2, double z2):
+    '''
+    Calculate the distance between points (x1,y1,z1) and (x2,y2,z2)
+    '''
+    return sqrt((x2-x1)**2, (y2-y1)**2, (z2-z1)**2)
+
+
+cpdef double volume(numpy.ndarray[numpy.float64_t, ndim=2] _solute_pos, 
+                    numpy.ndarray[numpy.float64_t, ndim=1] _solute_rad, 
+                    numpy.ndarray[numpy.float64_t, ndim=2] _solvent_pos, 
+                    double _solvent_rad,
+                    double _voxel_len):
+    '''
+    -----------
+    Parameters
+    -----------
+    _solute_pos: shape (n,3) array of (x,y,z) coordinates of solute atoms
+    _solute_rad: shape (n,) array of solute atom radii
+    _solvent_pos: shape (m,3) array of (x,y,z) coordinates of solvent atoms
+    _solvent_rad: solvent molecule radius; solvent is approximated as sphere
+    _voxel_len: edge length of voxels
+
+    -------------
+    Returns
+    -------------
+    The volume that is accessible to the centers of the water molecules
     '''
     cdef:
+        # Have Cython convert the arrays with a leading underscore from numpy
+        # arrays to C-style arrays
         double[:,:] solute_pos = _solute_pos
         double[:] solute_rad = _solute_rad
         double[:,:] solvent_pos = _solvent_pos
@@ -21,16 +86,24 @@ cpdef int volume(numpy.ndarray[numpy.float64_t, ndim=2] _solute_pos,
         int nx, ny, nz
         double x, y, z, X, Y, Z
         double solx, soly, solz
-        double solrad
         double solvent_floor[:,:]
         double solvent_ceil[:,:]
         bool check
 
 
-    box_buffer = 2*(solute_rad.max() + solvent_rad)
-    x_min, y_min, z_min = solute_pos.min(axis=1)
-    x_max, y_max, z_max = solute_pos.max(axis=1)
+    # We want the grids to be large enough that solvent can completely surround
+    # the solute; calculate a buffer size to do this.
+    box_buffer = 2*(_solute_rad.max() + solvent_rad)
+    x_min = min(_solute_pos[:,0].min(), _solvent_pos[:,0].min())
+    y_min = min(_solute_pos[:,1].min(), _solvent_pos[:,1].min())
+    z_min = min(_solute_pos[:,2].min(), _solvent_pos[:,2].min())
 
+    x_max = max(_solute_pos[:,0].max(), _solvent_pos[:,0].max())
+    y_max = max(_solute_pos[:,1].max(), _solvent_pos[:,1].max())
+    z_max = max(_solute_pos[:,2].max(), _solvent_pos[:,2].max())
+    
+
+    # the extreme values for the (rectangular) grid
     x_min -= box_buffer
     y_min -= box_buffer
     z_min -= box_buffer
@@ -38,12 +111,20 @@ cpdef int volume(numpy.ndarray[numpy.float64_t, ndim=2] _solute_pos,
     y_max += box_buffer
     z_max += box_buffer
 
-    nx = numpy.ceil((x_max - x_min)/voxel_len)
-    ny = numpy.ceil((x_max - x_min)/voxel_len)
-    nz = numpy.ceil((x_max - x_min)/voxel_len)
+    # Find the number of voxels to use; avoid fencepost error
+    #
+    #        * -- * -- * -- * -- * -- * -- *
+    #        1    2    3    4    5    6    7 
+    # 
+    nx = numpy.ceil((x_max - x_min)/voxel_len) + 1
+    ny = numpy.ceil((x_max - x_min)/voxel_len) + 1
+    nz = numpy.ceil((x_max - x_min)/voxel_len) + 1
+
     cdef bool[nx,ny,nz] grid = numpy.zeros((nx,ny,nz), dtype=bool)
     cdef bool[nx,ny,nz] visited_grid = numpy.zeros((nx,ny,nz), dtype=bool)
 
+    # shift the solute positions by the appropriate amount
+    # (will this work with C-style arrays?)
     solute_pos[:,0] -= xmin
     solute_pos[:,1] -= ymin
     solute_pos[:,2] -= zmin
@@ -241,23 +322,3 @@ cdef bool find_open(int[:,:] *open_arr, int *ix, int *iy, int *iz, int ngridpts)
             return True
     return False
 
-cdef bool is_free(double voxx, double voxy, double voxz,
-        double[:,:] solute_pos, double[:] solute_rad,
-        int nsolute):
-
-    cdef bool check = True
-
-    for isolute in range(nsolute):
-        solx = solute_pos[isolute,0]
-        soly = solute_pos[isolute,1]
-        solz = solute_pos[isolute,2]
-        solrad = solute_rad[isolute]
-        if dist(x,y,z,solx,soly,solz) < solrad + solvent_rad:
-            check = False
-            break
-    return check
-
-
-cdef double dist(double x1, double y1, double z1, 
-                 double x2, double y2, double z2):
-    return sqrt((x2-x1)**2, (y2-y1)**2, (z2-z1)**2)
