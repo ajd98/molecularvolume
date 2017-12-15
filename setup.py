a603208acb9b4004d003a5d2b2cbd22a7b11f12f
Alex DeGrave <ajd98@pitt.edu>
2017-12-14 18:35:43 -0800

    include librecursivefill.so

--- setup.py
+++ setup.py
@@ -1,8 +1,23 @@
 import numpy
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
+import os
+
+recursivefilldir = os.path.abspath(os.getcwd())
+print(recursivefilldir)
+
+ext_modules=[
+    Extension("volume",
+        sources=["volume.pyx"],
+        libraries=["m", "recursivefill"],
+        library_dirs=[recursivefilldir]
+    )
+]
+
 
 setup(
-    ext_modules=cythonize("volume.pyx"),
-    include_dirs=[numpy.get_include()]
+    name="volume",
+    ext_modules=cythonize(ext_modules),
+    include_dirs=[numpy.get_include()],
+    library_dirs=[recursivefilldir]
 )
