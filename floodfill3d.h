d597345ba0f74f8c0e6af18f5c190c8cdeaecf29
Alex DeGrave <ajd98@pitt.edu>
2017-12-14 23:45:37 -0800

    added header

---
+++ floodfill3d.h
@@ -0,0 +1,18 @@
+#ifndef RECURSIVEFILL_H 
+#define RECURSIVEFILL_H
+
+#include <math.h>
+
+double dist(double x1, double y1, double z1, double x2, double y2, double z2);
+
+unsigned char is_free(double voxx, double voxy, double voxz, double* solute_pos,
+                      double* solute_rad, int nsolute, double solvent_rad);
+
+void floofill(int ix, int iy, int iz, int nx, int ny, int nz, double voxel_len,
+              unsigned char* visited_grid, unsigned char* grid, int* moves,
+              double* solute_pos, double* solute_rad, int nsolute, 
+              double solvent_rad);
+
+#endif
+
+
