419a453621288085e2685c8149587fb11705561f
Alex DeGrave <ajd98@pitt.edu>
2017-12-15 00:00:31 -0800

    compile objects separately, then make library with ar

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 rm *.so
 export LIBRARY_PATH=$(pwd):${LIBRARY_PATH}
-gcc -O3 -c -fPIC floodfill3d.c -lm -o libfloodfill3d.so
+
+gcc -O3 -c floodfill3d.c -lm -o floodfill3d.o
+gcc -O3 -c queue.c -o queue.o
+ar cr libfloodfill3d.a queue.o floodfill3d.o
+
 python setup.py build_ext --inplace
 python test.py
