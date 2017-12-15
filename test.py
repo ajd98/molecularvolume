df5db2d84d0bb28977e18ad3cfc03c19c321da44
Alex DeGrave <ajd98@pitt.edu>
2017-12-14 21:49:54 -0800

    modified tests

--- test.py
+++ test.py
@@ -7,31 +7,51 @@ def main():
     print("testing...")
     solute_pos = numpy.array(((0,0,0),), dtype=numpy.float64)
     solute_rad = numpy.array((1,), dtype=numpy.float64)
-    solvent_pos = numpy.array(((5,0,0),), dtype=numpy.float64)
+    solvent_pos = numpy.array(((0,5,0),), dtype=numpy.float64)
 
     solvent_rad = 1.4
     voxel_len = 1.0
     vol = volume.volume(solute_pos, solute_rad, solvent_pos, solvent_rad, voxel_len)
     print(vol)
+
+
+    #solute_pos = numpy.array(((0,0,0),
+    #                          (1,0,0),
+    #                          (2,0,0),
+    #                          (3,0,0),
+    #                          (4,0,0),
+    #                          (5,0,0)), dtype=numpy.float64)
+    #solute_rad = numpy.array((1,
+    #                          1,
+    #                          1,
+    #                          1,
+    #                          1,
+    #                          1), dtype=numpy.float64)
+    #solvent_pos = numpy.array(((0,5,0),), dtype=numpy.float64)
+
+    #solvent_rad = 1.4
+    #voxel_len = 1.0
+    #vol = volume.volume(solute_pos, solute_rad, solvent_pos, solvent_rad, voxel_len)
+    #print(vol)
     
-    voxel_len = 0.9
-    vol = volume.volume(solute_pos, solute_rad, solvent_pos, solvent_rad, voxel_len)
-    print(vol)
+    #voxel_len = 0.9
+    #vol = volume.volume(solute_pos, solute_rad, solvent_pos, solvent_rad, voxel_len)
+    #print(vol)
 
-    voxel_len = 0.8
-    vol = volume.volume(solute_pos, solute_rad, solvent_pos, solvent_rad, voxel_len)
-    print(vol)
+    #voxel_len = 0.8
+    #vol = volume.volume(solute_pos, solute_rad, solvent_pos, solvent_rad, voxel_len)
+    #print(vol)
 
-    voxel_len = 0.5
-    vol = volume.volume(solute_pos, solute_rad, solvent_pos, solvent_rad, voxel_len)
-    print(vol)
+    #voxel_len = 0.5
+    #vol = volume.volume(solute_pos, solute_rad, solvent_pos, solvent_rad, voxel_len)
+    #print(vol)
 
-    cProfile.runctx("volume.volume(solute_pos, solute_rad, solvent_pos, solvent_rad, voxel_len)",
-                    globals(),
-                    locals(),
-                    "Profile.prof")
-    s = pstats.Stats("Profile.prof")
-    s.strip_dirs().sort_stats("time").print_stats()
+    #cProfile.runctx("volume.volume(solute_pos, solute_rad, solvent_pos, solvent_rad, voxel_len)",
+    #                globals(),
+    #                locals(),
+    #                "Profile.prof")
+    #s = pstats.Stats("Profile.prof")
+    #s.strip_dirs().sort_stats("time").print_stats()
 
 if __name__ == "__main__":
     main()
