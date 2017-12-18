04b859ad076c85564f83bbc98dbb7a9e278f7979
Alex DeGrave <ajd98@pitt.edu>
2017-12-18 11:27:22 -0800

    removed plotting

--- test/test.py
+++ test/test.py
@@ -12,17 +12,17 @@ def test_protein():
     vol, grid = pdb2volume.PDBVolume('villin.pdb', 
                                      'cavity.lib.autogen', voxel_len=0.5).run()
     print(vol)
-    fig = pyplot.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    w = numpy.where(grid==0)
-    ax.scatter(w[0], w[1], w[2], marker='o')
-    ax.set_xlim(0,grid.shape[0])
-    ax.set_ylim(0,grid.shape[1])
-    ax.set_zlim(0,grid.shape[2])
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_zlabel('z')
-    pyplot.show()
+    #fig = pyplot.figure()
+    #ax = fig.add_subplot(111, projection='3d')
+    #w = numpy.where(grid==0)
+    #ax.scatter(w[0], w[1], w[2], marker='o')
+    #ax.set_xlim(0,grid.shape[0])
+    #ax.set_ylim(0,grid.shape[1])
+    #ax.set_zlim(0,grid.shape[2])
+    #ax.set_xlabel('x')
+    #ax.set_ylabel('y')
+    #ax.set_zlabel('z')
+    #pyplot.show()
 
 def profile_protein():
     cProfile.runctx("pdb2volume.PDBVolume('villin.pdb', 'cavity.lib.autogen', voxel_len=0.5).run()",
