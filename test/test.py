7b4c5471436b6b96a545d251e2e3a5583848ef22
Alex DeGrave <ajd98@pitt.edu>
2017-12-19 11:47:45 -0800

    2 sphere test

--- test/test.py
+++ test/test.py
@@ -3,6 +3,67 @@ import pdb2volume
 import numpy
 import matplotlib.pyplot as pyplot
 from mpl_toolkits.mplot3d import Axes3D
+import sys
+sys.path.append('../')
+import volume
+
+def test_simple():
+    solute_pos = numpy.array(((0,0,0),), dtype=numpy.float64)
+    solute_rad = numpy.array(((3),), dtype=numpy.float64)
+    solvent_pos = numpy.array(((8,0,0),), dtype=numpy.float64)
+    solvent_rad = 1.4
+    voxel_len = 0.05
+
+    vol, grid = volume.volume(solute_pos, solute_rad, solvent_pos, solvent_rad, 
+                              voxel_len)
+    
+    analytical_vol = numpy.pi*4./3*(solute_rad[0]+solvent_rad)**3
+
+    print("Test: single atom of radius 3")
+    print("  calculated volume: {:f}".format(vol))
+    print("  analytical volume: {:f}".format(analytical_vol))
+    perr = 100*abs(vol-analytical_vol)/analytical_vol
+    print("  percent error:     {:f}%".format(perr))
+    if perr < 1:
+        print("  TEST PASSED")
+        return 0
+    else:
+        print("  TEST FAILED")
+        return 1
+
+def test_2sphere():
+    solute_pos = numpy.array(((0,0,0),
+                              (3,0,0)), dtype=numpy.float64)
+    sr = 3
+    solute_rad = numpy.array(((sr),
+                              (sr)), dtype=numpy.float64)
+
+    solvent_pos = numpy.array(((11,0,0),), dtype=numpy.float64)
+    solvent_rad = 1.4
+    voxel_len = 0.1
+
+    vol, grid = volume.volume(solute_pos, solute_rad, solvent_pos, solvent_rad, 
+                              voxel_len)
+    
+    # volume of sphere 0 + volume of sphere 1 - volume of intersection
+    h = (solute_rad[0]+solvent_rad)/2
+    r = solute_rad[0]+solvent_rad
+    analytical_vol = numpy.pi*4./3*r**3 \
+                   + numpy.pi*4./3*r**3 \
+                   - 2*1./3*numpy.pi*h**2*(3*r-h)
+
+    print("Test: two overlapping atoms of radius 3")
+    print("  calculated volume: {:f}".format(vol))
+    print("  analytical volume: {:f}".format(analytical_vol))
+    perr = 100*abs(vol-analytical_vol)/analytical_vol
+    print("  percent error:     {:f}%".format(perr))
+    if perr < 1:
+        print("  TEST PASSED")
+        return 0
+    else:
+        print("  TEST FAILED")
+        return 1
+    
 
 def test_protein():
     vol, grid = pdb2volume.PDBVolume('villin.pdb', 
@@ -26,4 +87,5 @@ def show_protein_surface():
     pyplot.show()
 
 if __name__ == "__main__":
-    test_protein()
+    test_simple()
+    test_2sphere()
