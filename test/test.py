8db63d508f10ffbdbf2c37f166173c9f180b80f6
Alex DeGrave <ajd98@pitt.edu>
2017-12-20 13:35:18 -0800

    test nonoverlapping spheres

--- test/test.py
+++ test/test.py
@@ -58,6 +58,36 @@ def test_simple_overlap():
 
 def test_2sphere():
     solute_pos = numpy.array(((0,0,0),
+                              (8,0,0)), dtype=numpy.float64)
+    sr = 3
+    solute_rad = numpy.array(((sr),
+                              (sr)), dtype=numpy.float64)
+
+    solvent_pos = numpy.array(((16,0,0),), dtype=numpy.float64)
+    solvent_rad = 1.4
+    voxel_len = 0.03
+
+    print("Test: two non-overlapping atoms of radius 3")
+    vol = volume.volume(solute_pos, solute_rad, solvent_pos, solvent_rad, 
+                              voxel_len)
+    
+    r = solute_rad[0]+solvent_rad
+    analytical_vol = numpy.pi*4./3*r**3 \
+                   + numpy.pi*4./3*r**3 
+
+    print("  calculated volume: {:f}".format(vol))
+    print("  analytical volume: {:f}".format(analytical_vol))
+    perr = 100*abs(vol-analytical_vol)/analytical_vol
+    print("  percent error:     {:f}%".format(perr))
+    if perr < 1:
+        print("  TEST PASSED")
+        return 0
+    else:
+        print("  TEST FAILED")
+        return 1
+
+def test_2sphere_overlapping():
+    solute_pos = numpy.array(((0,0,0),
                               (3,0,0)), dtype=numpy.float64)
     sr = 3
     solute_rad = numpy.array(((sr),
