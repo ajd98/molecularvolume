16f44a08b8dd70c7b08267117ac21ee6f0572950
Alex DeGrave <ajd98@pitt.edu>
2018-04-25 08:18:51 -0700

    use restrict type qualifier

--- src/floodfill3d.c
+++ src/floodfill3d.c
@@ -15,7 +15,8 @@ double dist(double x1, double y1, double z1, double x2, double y2, double z2) {
 
 
 unsigned char is_free(double voxx, double voxy, double voxz, 
-                      const double* solute_pos, const double* solute_rad, 
+                      const double* restrict solute_pos, 
+                      const double* restrict solute_rad, 
                       int nsolute, double solvent_rad)
 {
     /*
@@ -69,8 +70,10 @@ unsigned char is_free(double voxx, double voxy, double voxz,
 
 
 void floodfill(int ix, int iy, int iz, int nx, int ny, int nz, double voxel_len,
-               unsigned char* visited_grid, unsigned char* grid, int* moves,
-               double* solute_pos, double* solute_rad, int nsolute, 
+               unsigned char* restrict visited_grid, 
+               unsigned char* restrict grid, const int* restrict moves,
+               const double* restrict solute_pos, 
+               const double* restrict solute_rad, int nsolute, 
                double solvent_rad)
 {
     /*
