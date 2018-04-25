fb5793d20af96005df0391d670cdf3327061abf5
Alex DeGrave <ajd98@pitt.edu>
2018-04-25 08:08:58 -0700

    Use const type qualifier

--- src/floodfill3d.c
+++ src/floodfill3d.c
@@ -14,8 +14,9 @@ double dist(double x1, double y1, double z1, double x2, double y2, double z2) {
 }
 
 
-unsigned char is_free(double voxx, double voxy, double voxz, double* solute_pos,
-                      double* solute_rad, int nsolute, double solvent_rad)
+unsigned char is_free(double voxx, double voxy, double voxz, 
+                      const double* solute_pos, const double* solute_rad, 
+                      int nsolute, double solvent_rad)
 {
     /*
      * ---------------
