e30748460eec34cae695f0a4363515c13a53b8b8
Alex DeGrave <ajd98@pitt.edu>
2018-05-01 11:42:41 -0700

    Update formatting for function declarations

--- src/floodfill3d.c
+++ src/floodfill3d.c
@@ -3,7 +3,9 @@
 #include <stdlib.h>
 #include <limits.h>
 
-double dist(double x1, double y1, double z1, double x2, double y2, double z2) {
+double 
+dist(double x1, double y1, double z1, double x2, double y2, double z2)
+{
     /*
      * Calculate the distance between (x1, y1, z1) and (x2, y2, z2)
      */ 
@@ -14,10 +16,10 @@ double dist(double x1, double y1, double z1, double x2, double y2, double z2) {
 }
 
 
-unsigned char is_free(double voxx, double voxy, double voxz, 
-                      const double* restrict solute_pos, 
-                      const double* restrict solute_rad, 
-                      int nsolute, double solvent_rad)
+unsigned char 
+is_free(double voxx, double voxy, double voxz, 
+        const double* restrict solute_pos, const double* restrict solute_rad, 
+        int nsolute, double solvent_rad)
 {
     /*
      * ---------------
@@ -69,12 +71,11 @@ unsigned char is_free(double voxx, double voxy, double voxz,
 }
 
 
-void floodfill(int ix, int iy, int iz, int nx, int ny, int nz, double voxel_len,
-               unsigned char* restrict visited_grid, 
-               unsigned char* restrict grid, const int* restrict moves,
-               const double* restrict solute_pos, 
-               const double* restrict solute_rad, int nsolute, 
-               double solvent_rad)
+void 
+floodfill(int ix, int iy, int iz, int nx, int ny, int nz, double voxel_len,
+          unsigned char* restrict visited_grid, unsigned char* restrict grid, 
+          const int* restrict moves, const double* restrict solute_pos,
+          const double* restrict solute_rad, int nsolute, double solvent_rad)
 {
     /*
      * ----------
